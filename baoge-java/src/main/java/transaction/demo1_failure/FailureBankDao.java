e7051bb71e796a4b3e6327349c50d28ad1f5d61c
shaoxubao <shaoxubao-sz@fangdd.com>
2018-10-04 00:06:47 +0800

    [ADD]事务demo1_failure

---
+++ baoge-java/src/main/java/transaction/demo1_failure/FailureBankDao.java
@@ -0,0 +1,44 @@
+package transaction.demo1_failure;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * Copyright 2018-2028 Baoge All Rights Reserved.
+ * Author: Shao Xu Bao <15818589952@163.com>
+ * Date:   2018/10/3
+ */
+public class FailureBankDao {
+
+    private DataSource dataSource;
+
+    public FailureBankDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    public void withdraw(int bankId, int amount) throws Exception {
+        Connection connection = dataSource.getConnection();
+        PreparedStatement selectStatement = connection.prepareStatement("SELECT BANK_AMOUNT FROM BANK_ACCOUNT WHERE BANK_ID = ?");
+        selectStatement.setInt(1, bankId);
+
+        ResultSet resultSet = selectStatement.executeQuery();
+        resultSet.next();
+        int previousAmount = resultSet.getInt(1);
+        resultSet.close();
+        selectStatement.close();
+
+        int newAmount = previousAmount - amount;
+        PreparedStatement updateStatement = connection.prepareStatement("UPDATE BANK_ACCOUNT SET BANK_AMOUNT = ? WHERE BANK_ID = ?");
+        updateStatement.setInt(1, newAmount);
+        updateStatement.setInt(2, bankId);
+        updateStatement.execute();
+
+        updateStatement.close();
+        connection.close();
+
+    }
+
+}
---
+++ baoge-java/src/test/transaction/demo1_failure/FailureBankServiceTest.java
@@ -0,0 +1,51 @@
+package transaction.demo1_failure;
+
+import org.junit.Test;
+import transaction.BankFixture;
+
+import java.sql.SQLException;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Copyright 2018-2028 Baoge All Rights Reserved.
+ * Author: Shao Xu Bao <15818589952@163.com>
+ * Date:   2018/10/4
+ */
+public class FailureBankServiceTest extends BankFixture {
+
+    @Test
+    public void transferSuccess() throws SQLException
+    {
+        FailureBankDao failureBankDao = new FailureBankDao(dataSource);
+        FailureInsuranceDao failureInsuranceDao = new FailureInsuranceDao(dataSource);
+
+        FailureBankService bankService = new FailureBankService(dataSource);
+        bankService.setFailureBankDao(failureBankDao);
+        bankService.setFailureInsuranceDao(failureInsuranceDao);
+
+        bankService.transfer(1111, 2222, 200);
+
+        assertEquals(800, getBankAmount(1111));
+        assertEquals(1200, getInsuranceAmount(2222));
+
+    }
+
+    @Test
+    public void transferFailure() throws SQLException
+    {
+        FailureBankDao failureBankDao = new FailureBankDao(dataSource);
+        FailureInsuranceDao failureInsuranceDao = new FailureInsuranceDao(dataSource);
+
+        FailureBankService bankService = new FailureBankService(dataSource);
+        bankService.setFailureBankDao(failureBankDao);
+        bankService.setFailureInsuranceDao(failureInsuranceDao);
+
+        int toNonExistId = 3333;
+        bankService.transfer(1111, toNonExistId, 200);
+
+        assertEquals(1000, getInsuranceAmount(2222));
+        assertEquals(800, getBankAmount(1111));
+    }
+
+}
