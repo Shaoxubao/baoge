8b1ee55d5973fa57af907595343fa8bd08361e93
shaoxubao <shaoxubao-sz@fangdd.com>
2018-10-04 23:40:57 +0800

    [ADD]事务——代理、注解实现

---
+++ baoge-java/src/main/java/transaction/demo6_annotation/Transactional.java
@@ -0,0 +1,17 @@
+package transaction.demo6_annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Copyright 2018-2028 Baoge All Rights Reserved.
+ * Author: Shao Xu Bao <15818589952@163.com>
+ * Date:   2018/10/4
+ */
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Transactional {
+}
---
+++ baoge-java/src/test/transaction/demo6_annotation/AnnotationBankServiceTest.java
@@ -0,0 +1,42 @@
+package transaction.demo6_annotation;
+
+import org.junit.Test;
+import transaction.BankFixture;
+import transaction.BankService;
+import transaction.demo3_transaction_holder.TransactionManager;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Copyright 2018-2028 Baoge All Rights Reserved.
+ * Author: Shao Xu Bao <15818589952@163.com>
+ * Date:   2018/10/4
+ */
+public class AnnotationBankServiceTest extends BankFixture {
+
+    @Test
+    public void transferSuccess() throws Exception {
+        TransactionManager transactionManager = new TransactionManager(dataSource);
+        TransactionEnabledAnnotationProxyManager transactionEnabledProxyManager = new TransactionEnabledAnnotationProxyManager(transactionManager);
+        BankService bankService = new AnnotationBankService(dataSource);
+        BankService proxyBankService = (BankService) transactionEnabledProxyManager.proxyFor(bankService);
+        proxyBankService.transfer(1111, 2222, 200);
+
+        assertEquals(800, getBankAmount(1111));
+        assertEquals(1200, getInsuranceAmount(2222));
+    }
+
+    @Test
+    public void transferFailure() throws Exception {
+        TransactionEnabledAnnotationProxyManager transactionEnabledAnnotationProxyManager = new TransactionEnabledAnnotationProxyManager(new TransactionManager(dataSource));
+        BankService bankService = new AnnotationBankService(dataSource);
+        BankService proxyBankService = (BankService) transactionEnabledAnnotationProxyManager.proxyFor(bankService);
+
+        int toNonExistId = 3333;
+        proxyBankService.transfer(1111, toNonExistId, 200);
+
+        assertEquals(1000, getBankAmount(1111));
+        assertEquals(1000, getInsuranceAmount(2222));
+    }
+
+}
