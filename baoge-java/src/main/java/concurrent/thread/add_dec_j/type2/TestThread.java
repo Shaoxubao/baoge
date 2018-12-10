0a462b1bab008105c129a3bf5c47fc6c1da30bef
shaoxubao <shaoxubao-sz@fangdd.com>
2018-12-10 14:03:10 +0800

    [OPT]添加注释

--- baoge-java/src/main/java/concurrent/thread/add_dec_j/type2/TestThread.java
+++ baoge-java/src/main/java/concurrent/thread/add_dec_j/type2/TestThread.java
@@ -2,6 +2,10 @@ package concurrent.thread.add_dec_j.type2;
 
 import concurrent.thread.add_dec_j.MyData;
 
+/**
+ * 与第一种方法TestOne的区别在于第二种方法巧妙的用了内部类共享外部类数据的思想，即把要共享的数据变得全局变量，
+ * 这样就保证了操作的是同一份数据。同时内部类的方式使代码更加简洁。但是不如第一种解法条理那么清楚。
+ */
 public class TestThread {
 
     public static void main(String[] args) {
