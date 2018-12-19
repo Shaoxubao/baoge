2e225b6b9d4913a39b3b379f72095a53278949f2
shaoxubao <shaoxubao-sz@fangdd.com>
2018-12-19 14:02:21 +0800

    [OPT]将一篇文章按照标题拆解成变量优化

--- baoge-scala/src/test/scala/BaikeVariationJob.scala
+++ baoge-scala/src/test/scala/BaikeVariationJob.scala
@@ -11,49 +11,45 @@ object BaikeVariationJob {
 
     def main(args: Array[String]): Unit = {
 
-        val title = "定金可以退还吗？定金什么情况下可以退？"
-        val content = "<p>我们大家在买房子的时候，有时候开发商会要求我们是需要缴纳一部分的定金的，这种情况下，一般的购房者们都会在冲动之下交了钱，但有的人过后又会后悔，不想要买这套房子了，或者开发商不想把房子卖给购房者了。那么，定金可以退还吗？定金什么情况下可以退？</p><p><img src=\"https://static-tp.com/xfwf/Fo0J-5t7CEydWA51ZAK1Cry8O5kJ.jpg\"></p><p><strong>定金可以退还吗？</strong></p><p>1、退定金是有一定条件的，即因合同条款达不成一致意见可以退，另外，必须是在认购书约定的期限内来签合同，要想退定金必须要证明这一点。购房人可以通过合同条件谈判时双方修改的记录或者双方谈话的录音来证明。</p><p>2、到目前为止，涉及到定金的法律有以下一些：《合同法》、《担保法》、《担保法司法解释》、《商品房买卖合同纠纷司法解释》等法律。</p><p>3、但是所有的法律关于定金这的规定都是一样的：给付定金的一方如果没有按照合同的约定履行相关的义务或者是债务的，无权要求返还定金；收受定金的一方不履行约定的债务的，应当双倍返还定金。</p><p>4、具体到房屋买卖上来说：出卖人通过认购、订购、预订等方式向买受人收受定金作为订立商品房买卖合同担保的，如果因当事人一方原因未能订立商品房买卖合同，应当按照法律关于定金的规定处理；因不可归责于当事人双方的事由，导致商品房买卖合同未能订立的，出卖人应当将定金返还买受人。</p><p><img src=\"https://static.com/xfwf/FkQwuEDv3YRJZ7RZwQe3hD6FXpqm.jpg\"></p><p><strong>定金什么情况下可以退？</strong></p><p>1、开发商无销售许可证或者产权证，不具备商品房销售资格，导致购房合同无法达成。</p><p>2、开发商存在欺诈行为，如已将认购房屋转售他人。</p><p>3、因合同条款达不成一致意见。如卖家对房屋信息表述模糊，合同主要条款无法确定，或者卖家对认购书中的条件，如房屋面积、价格等进行修改等。</p><p>4、买卖双方均无过错，但商品房买卖合同依旧未能订立。如因第三人的原因或自然灾害，致使该商品房项目未能建设或未能按原约定建设等。</p><p>以上即是有关于定金可以退还吗以及定金什么情况下可以退的详细解读了，希望能够对大家有所帮助。通过文章我们不难看出，即便是定金可以退，当然也是要满足一定的条件才可以，不是随随便便就能够把定金退掉的。当然，退定金的过程也非常麻烦，大家在交定金前最好考虑清楚。</p>"
+        val title = "父母买房如何获得子女公积金？获得公积金需要什么证明？"
+        val content = "<p>现在很多人购房都会使用公积金贷款，因为公积金贷款比较稳定，是国家给予职工的福利，那么父母买房如何获得子女公积金？获得公积金需要什么证明？其实父母不可以使用子女的公积金，但是具体原因还是需要依据一定的法律条件，住房资金管理规定，在职职工个人贷款的共同申请人，定是借款人的配偶，那么今天小编就来谈一谈这个问题。</p><p><img src=\"https://static-tp.fangdd.com/xfwf/FtkfixLDNcEaWocAVntbQFcwJHqL.jpg\"></p><p><strong>父母买房如何获得子女公积金？</strong></p><p>1、那虽然无法获得子女的公积金，但是公积金的具体获得方式如下：我们根据目前城市公积金退出的政策，符合购买本市以外自有住房条件的职工为本人及其配偶获得公积金。符合条件的员工是指户籍证明或在所购房屋所在城市工作。证明。购买外国房屋时，如果是全额购买房屋，需提供自己的配偶身份证，公积金卡，购房合同，房产证，户口本和户籍证明或工作证明。</p><p>2、我们参加住房公积金制度者要申请住房公积金个人购房贷款还必须符合以下条件：即申请贷款前连续缴存住房公积金的时间不少于六个月。因为，如果职工缴存住房公积金的行为不正常，时断时续，说明其收入不稳定，发放贷款后容易产生风险。</p><p>3、其配偶一方申请了住房公积金贷款，在其未还清贷款本息之前，配偶双方均不能再获得住房公积金贷款。因为，住房公积金贷款是满足职工家庭住房基本需求时提供的金融支持，是一种＂住房保障型＂的金融支持。</p><p><img src=\"https://static-tp.fangdd.com/xfwf/FghgGquEWrD4rRplqNNbBzNc_tSD.jpg\"></p><p><strong>获得公积金需要什么证明？</strong></p><p>那虽然无法获得子女的公积金，但是公积金的具体需要的证明如下：那如果员工是贷款买方，除了上述信息外，他还必须提供贷款合同和还款证明。购买房屋的工人可以一次性提取第1笔付款，或者可以按月偿还贷款。工人拿起第1笔付款后，他／她再次偿还每月付款。取款只需要提供公积金卡，身份证和还款证。</p><p>以上就是小编介绍的父母买房如何获得子女公积金？获得公积金需要什么证明？这些问题的回答，如果你觉得过于难以理解不妨再看几遍。公积金的具体使用方法还是需要具备一定条件的，自己多了解这些内容也好过吃了不了解的亏。具体的使用方法还需要依据当地的政策来定，也很感谢你的支持与阅读，如果你有什么疑问我们也将竭诚为您解答。</p>"
 
-        val titleArr = title.split("？")
+        val titleArr = title.split("？|\\?")
         val title_a = titleArr(0)
         val title_b = titleArr(1)
         if (titleArr.length == 2 && content.contains(title_a + "？")
-        && content.contains(title_b + "？")) {
+                && content.contains(title_b + "？")) {
             var contentVar_a = ""
             var contentVar_b = ""
-            val reg1 = "<strong>" + title_a + "？"
-            val reg2 = "<strong>" + title_b + "？"
+            val reg1 = "<p><strong>" + title_a + "？</strong></p>"
+            val reg2 = "<p><strong>" + title_b + "？</strong></p>"
             var contentSegment = content.split(reg1)
-            contentSegment = contentSegment(1).split(reg2)
             if (contentSegment != null && contentSegment.length == 2) {
-                contentVar_a = contentSegment(0)
-                contentVar_b = contentSegment(1)
+                contentSegment = contentSegment(1).split(reg2)
+                if (contentSegment != null && contentSegment.length == 2) {
+                    contentVar_a = contentSegment(0)
+                    contentVar_b = contentSegment(1)
 
-                contentVar_a = contentVar_a.replaceAll(title_a + "？", "")
-                        .replaceAll("</strong></p>", "")
+                    val contentVar_b_doc = Jsoup.parse(contentVar_b)
 
-                contentVar_b = contentVar_b.replaceAll(title_b + "？", "")
-                        .replaceAll("</strong></p>", "")
+                    val contentVar_b_eles = contentVar_b_doc.body.getElementsByTag("p")
+                    // 有的需要去掉最后一段
+                    if (contentVar_b_eles != null) {
+                        contentVar_b = ""
+                        var resultSize = 0
+                        val size = contentVar_b_eles.size()
+                        val segCom = contentVar_b_eles.get(contentVar_b_eles.size() - 2)
+                        val com = segCom.text().substring(0, 1)
+                        if (segCom.toString.contains("<strong>") && num.contains(com)) resultSize = size - 1 else resultSize = size - 2
 
-                val contentVar_b_doc = Jsoup.parse(contentVar_b)
-
-                val contentVar_b_eles = contentVar_b_doc.body.getElementsByTag("p")
-                // 有的需要去掉最后一段
-                if (contentVar_b_eles != null) {
-                    contentVar_b = ""
-                    var size = contentVar_b_eles.size()
-                    val segCom = contentVar_b_eles.get(contentVar_b_eles.size() -2).text()
-                    val com = segCom.charAt(0)
-                    if (!segCom.contains("<strong>") && (num.contains(com))) {
-                        size = size -2
+                        for (i <- 0 to resultSize) {
+                            contentVar_b = contentVar_b + contentVar_b_eles.get(i).toString
+                        }
                     }
-                    for (i <- 0 to size) {
-                        contentVar_b = contentVar_b + contentVar_b_eles.get(i).toString
-                    }
+
+                    println("contentVar_a:" + contentVar_a + "\n")
+                    println("contentVar_b:" + contentVar_b + "\n")
                 }
-
-                println("contentVar_a:" + contentVar_a + "\n")
-                println("contentVar_b:" + contentVar_b + "\n")
             }
 
         }
