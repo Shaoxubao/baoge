10c19004f907d3a04c3f4d90e9d6ce8b4383025f
shaoxubao <shaoxubao-sz@fangdd.com>
2018-12-27 17:43:13 +0800

    [ADD]修改调用百度api版本号

--- baoge-java/src/main/java/bigdata/nlp/AipNlpClient.java
+++ baoge-java/src/main/java/bigdata/nlp/AipNlpClient.java
@@ -26,7 +26,12 @@ public class AipNlpClient {
         JSONObject obj = JSONObject.fromObject(jsonObject.toString());
         NlpResult nlpResult = (NlpResult) JSONObject.toBean(obj, NlpResult.class);
 
-        System.out.println(nlpResult);
+//        System.out.println(nlpResult);
+
+        String content = "大多数的床头台灯都为工艺台灯，是由灯座和灯罩二部分组成。灯座由陶瓷、石料、景泰蓝、竹编、大理石等材料制作。灯罩常用玻璃、金属、塑料、织物竹藤做成，灯座和灯罩一经巧妙地，便使台灯成为美丽的艺术品。 1、光谱成分中没有紫外光和红外光。 2、光的色温应贴近自然 3、灯光为无频闪光 在选购台灯时，可随身带一个小陀螺，在灯光下旋转陀螺，如果没有产生倒转的视错觉，就说明灯具没有频闪。";
+        jsonObject = client.newsSummary(content, 200, null);
+        System.out.println(jsonObject);
+
     }
 
 }
