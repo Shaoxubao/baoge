023c71ed936064f32ee47144d7a4cdfe81718e25
shaoxubao <shaoxubao-sz@fangdd.com>
2018-12-29 18:13:38 +0800

    [ADD]MyArrayList init

--- baoge-java/src/main/java/jdk8/arraylist/MyArrayList.java
+++ baoge-java/src/main/java/jdk8/arraylist/MyArrayList.java
@@ -27,7 +27,18 @@ public class MyArrayList {
     }
 
     public MyArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.elementData = new Object[initialCapacity];
+        } else if (initialCapacity == 0) {
+            this.elementData = EMPTY_ELEMENTDATA;
+        } else {
+            throw new IllegalArgumentException("illegal argument:" + initialCapacity);
+        }
+    }
 
+    public boolean add(Object e) {
+
+        return true;
     }
 
 
