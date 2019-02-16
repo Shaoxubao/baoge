c7916eff0d625b2667ec7fccfa325015d3f63cad
shaoxubao <shaoxubao-sz@fangdd.com>
2019-02-16 20:45:19 +0800

    线程死锁demo 加注释

--- baoge-java/src/main/java/concurrent/thread/deadlock/DeadLockDemo.java
+++ baoge-java/src/main/java/concurrent/thread/deadlock/DeadLockDemo.java
@@ -5,6 +5,18 @@ package concurrent.thread.deadlock;
  * Author: Shao Xu Bao <15818589952@163.com>
  * Date:   2019/2/16
  * Desc:   写一段死锁代码
+ * 线程死锁是指由于两个或者多个线程互相持有对方所需要的资源,导致这些线程处于等待状态,无法前往执行。
+ * 线程Thread1率先占有了lock1, 继续运行时需要lock2, 但此时lock2却被线程Thread2占有了，
+ * 因此只能等待Thread2释放lock2才能够继续运行； 同时，Thread2也需要lock1,
+ * 它只能等待Thread1释放lock1才能够继续运行， 因此，Thread1和Thread2都处于等待状态，
+ * 谁也无法继续运行，即产生了死锁。
+ *
+ * 产生死锁还有另一种递归方式
+ *
+ * 三种用于避免死锁的技术：
+ * 加锁顺序
+ * 加锁时限
+ * 死锁检测
  */
 public class DeadLockDemo {
 
