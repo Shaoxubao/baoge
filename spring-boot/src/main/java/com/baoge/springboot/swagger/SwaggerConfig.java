ce8df75d4d8a172680324df7a1953a48da500cdf
邵绪宝 <shaoxb@yunjiglobal.com>
2019-05-10 16:39:57 +0800

    修改设置

--- spring-boot/src/main/java/com/baoge/springboot/swagger/SwaggerConfig.java
+++ spring-boot/src/main/java/com/baoge/springboot/swagger/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.baoge.springboot.swagger;
 
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -21,6 +25,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
+    // 定义分隔符,配置Swagger多包
+    private static final String splitor = ";";
+
     @Bean
     public Docket swaggerSpringMvcPlugin() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -28,6 +35,8 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .groupName("用户管理")
                 .select()
                 .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+//                .apis(basePackage("com.baoge.springboot.guaua"
+//                        + splitor + "com.baoge.springboot.swagger"))
                 .build();
     }
 
@@ -52,6 +61,39 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
 
         super.addResourceHandlers(registry);
+    }
+
+    /**
+     * 重写basePackage方法，使能够实现多包访问
+     * @param basePackage
+     * @return com.google.common.base.Predicate<springfox.documentation.RequestHandler>
+     */
+    public static Predicate<RequestHandler> basePackage(final String basePackage) {
+        return new Predicate<RequestHandler>() {
+            @Override
+            public boolean apply(RequestHandler input) {
+                return declaringClass(input).transform(handlerPackage(basePackage)).or(true);
+            }
+        };
+    }
+
+    private static Function<Class<?>, Boolean> handlerPackage(final String basePackage)     {
+        return new Function<Class<?>, Boolean>() {
+            @Override
+            public Boolean apply(Class<?> input) {
+                for (String strPackage : basePackage.split(splitor)) {
+                    boolean isMatch = input.getPackage().getName().startsWith(strPackage);
+                    if (isMatch) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        };
+    }
+
+    private static Optional<? extends Class<?>> declaringClass(RequestHandler input) {
+        return Optional.fromNullable(input.declaringClass());
     }
 
 }
