8aec45d01e5a2553fa8e5aaf7d8e6e04ad99906f
邵绪宝 <shaoxb@yunjiglobal.com>
2019-05-10 11:50:23 +0800

    修改继承类

--- springboot/src/main/java/com/baoge/springboot/swagger/SwaggerConfig.java
+++ springboot/src/main/java/com/baoge/springboot/swagger/SwaggerConfig.java
@@ -4,7 +4,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -19,7 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurerAdapter {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket swaggerSpringMvcPlugin() {
@@ -32,7 +32,10 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("swagger RESTful APIs").description("swagger RESTful APIs").termsOfServiceUrl("http://www.test.com/").contact("xiaoymin@foxmail.com").version("1.0").build();
+        return new ApiInfoBuilder().title("swagger RESTful APIs").description("swagger RESTful APIs")
+                .termsOfServiceUrl("http://www.test.com/")
+                .contact("15818589952@163.com")
+                .version("1.0.0").build();
     }
 
     // 添加ResourceHandler
