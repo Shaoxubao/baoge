2c9c7af3425458f7231560a46c2151463ca7fb5c
邵绪宝 <shaoxb@yunjiglobal.com>
2019-05-17 09:57:30 +0800

    雪花算法

--- baoge-java/src/main/java/algorithm/dfa/SensitiveWordInit.java
+++ baoge-java/src/main/java/algorithm/dfa/SensitiveWordInit.java
@@ -1,4 +1,4 @@
-package dfa;
+package algorithm.dfa;
 
 import java.io.BufferedReader;
 import java.io.File;
--- baoge-java/src/main/java/algorithm/dfa/SensitivewordFilter.java
+++ baoge-java/src/main/java/algorithm/dfa/SensitivewordFilter.java
@@ -1,4 +1,4 @@
-package dfa;
+package algorithm.dfa;
 
 import java.util.HashSet;
 import java.util.Iterator;
