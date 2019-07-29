ceb0db979312b181b2f605d9eaea82f3e959b8c4
邵绪宝 <shaoxb@yunjiglobal.com>
2019-07-29 15:34:31 +0800

    正则 demo注释

--- baoge-java/src/main/java/regex/RegexDemo.java
+++ baoge-java/src/main/java/regex/RegexDemo.java
@@ -1,18 +1,13 @@
 package regex;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class RegexDemo {
 
     public static void main(String[] args) {
 
+        // 以...开头正则表达
         String rex = "^ABC.*";
 
         String[] arr = {"ABCLP9", "ANB1233", "ABCdksk"};
-
 
         for (int i = 0; i < arr.length; i++) {
             String item = arr[i];
