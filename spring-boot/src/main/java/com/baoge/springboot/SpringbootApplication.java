c97858b50fda72e7848cc0bec3f208386a8a066a
shaoxubao <shaoxubao-sz@fangdd.com>
2019-08-09 18:11:21 +0800

    注释

--- spring-boot/src/main/java/com/baoge/springboot/SpringbootApplication.java
+++ spring-boot/src/main/java/com/baoge/springboot/SpringbootApplication.java
@@ -3,6 +3,9 @@ package com.baoge.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @SpringBootApplication标注一个主程序类，说明是一个Spring Boot应用
+ */
 @SpringBootApplication
 public class SpringbootApplication {
 
