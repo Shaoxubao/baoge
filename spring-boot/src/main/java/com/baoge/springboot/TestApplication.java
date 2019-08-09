1832e281d1c59f3ad299e5927790e3c56cf7dbab
shaoxubao <shaoxubao-sz@fangdd.com>
2019-08-09 16:26:54 +0800

    注释

--- spring-boot/src/main/java/com/baoge/springboot/TestApplication.java
+++ spring-boot/src/main/java/com/baoge/springboot/TestApplication.java
@@ -1,17 +1,17 @@
-package com.baoge.springboot;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@SpringBootApplication
-@MapperScan("com.baoge.springboot.mapper")
-@EnableScheduling
-public class TestApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
-
-}
+//package com.baoge.springboot;
+//
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//
+//@SpringBootApplication
+//@MapperScan("com.baoge.springboot.mapper")
+//@EnableScheduling
+//public class TestApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(TestApplication.class, args);
+//	}
+//
+//}
