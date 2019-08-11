1996f1f8e8822e340b3a5dfeb76baec21f935037
shaoxubao <shaoxubao-sz@fangdd.com>
2019-08-11 18:43:01 +0800

    SpringBoot自动配置原理

---
+++ spring-boot-autoconfig/src/main/java/com/baoge/springboot/SpringBootAutoconfigApplication.java
@@ -0,0 +1,13 @@
+package com.baoge.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutoconfigApplication.class, args);
+    }
+
+}
