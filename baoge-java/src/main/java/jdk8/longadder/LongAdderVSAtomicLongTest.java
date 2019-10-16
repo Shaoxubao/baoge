b2b0a885144b60ea9b00be472a63a4717e84f29a
邵绪宝 <shaoxb@yunjiglobal.com>
2019-10-16 09:56:36 +0800

    LongAdder vs AtomicLong demo

---
+++ baoge-java/src/main/java/jdk8/longadder/LongAdderVSAtomicLongTest.java
@@ -0,0 +1,79 @@
+package jdk8.longadder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * jdk 1.8
+ * LongAddEr vs AtomicLong
+ * 可以看到当只有一个线程的时候，AtomicLong反而性能更高，随着线程越来越多，AtomicLong的性能急剧下降，而LongAdder的性能影响很小。
+ *
+ * @Author shaoxubao
+ * @Date 2019/10/16 9:41
+ */
+public class LongAdderVSAtomicLongTest {
+    public static void main(String[] args) {
+        testAtomicLongVSLongAdder(1, 10000000);
+        testAtomicLongVSLongAdder(10, 10000000);
+        testAtomicLongVSLongAdder(20, 10000000);
+        testAtomicLongVSLongAdder(40, 10000000);
+        testAtomicLongVSLongAdder(80, 10000000);
+    }
+
+    static void testAtomicLongVSLongAdder(final int threadCount, final int times) {
+        try {
+            System.out.println("threadCount：" + threadCount + ", times：" + times);
+            long start = System.currentTimeMillis();
+            testLongAdder(threadCount, times);
+            System.out.println("LongAdder elapse：" + (System.currentTimeMillis() - start) + "ms");
+
+            long start2 = System.currentTimeMillis();
+            testAtomicLong(threadCount, times);
+            System.out.println("AtomicLong elapse：" + (System.currentTimeMillis() - start2) + "ms");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void testAtomicLong(final int threadCount, final int times) throws InterruptedException {
+        AtomicLong atomicLong = new AtomicLong();
+        List<Thread> list = new ArrayList<>();
+        for (int i=0; i < threadCount; i++) {
+            list.add(new Thread(() -> {
+                for (int j = 0; j < times; j++) {
+                    atomicLong.incrementAndGet();
+                }
+            }));
+        }
+
+        for (Thread thread : list) {
+            thread.start();
+        }
+
+        for (Thread thread : list) {
+            thread.join();
+        }
+    }
+
+    static void testLongAdder(final int threadCount, final int times) throws InterruptedException {
+        LongAdder longAdder = new LongAdder();
+        List<Thread> list = new ArrayList<>();
+        for (int i=0; i < threadCount; i++) {
+            list.add(new Thread(() -> {
+                for (int j = 0; j < times; j++) {
+                    longAdder.add(1);
+                }
+            }));
+        }
+
+        for (Thread thread : list){
+            thread.start();
+        }
+
+        for (Thread thread : list){
+            thread.join();
+        }
+    }
+}
