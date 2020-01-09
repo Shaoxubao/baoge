5b2353c1a69260a97fa4d432d3f272e34fbadcc3
邵绪宝 <shaoxb@yunjiglobal.com>
2020-01-09 14:56:00 +0800

    Z 字形变换

--- baoge-java/src/main/java/algorithm/zigzag_conversion/ZigZagConversion.java
+++ baoge-java/src/main/java/algorithm/zigzag_conversion/ZigZagConversion.java
@@ -38,6 +38,7 @@ public class ZigZagConversion {
 
     public static void main(String[] args) {
         System.out.println(convert("LEETCODEISHIRING", 3));
+        System.out.println(convert1("LEETCODEISHIRING", 3));
     }
 
     /**
@@ -68,6 +69,61 @@ public class ZigZagConversion {
             sbs[0].append(sbs[row]);
 
         return sbs[0].toString();
+    }
+
+    /**
+     * 当排列行数为 n=3 时：（没加括号的数字表示在原字符串中的下标，加括号的数字为相邻两数字的差）
+     *
+     * 0 (4) 4 (4)       8   12
+     * 1 (2) 3 (2) 5 (2) 7 9 11
+     * 2 (4)       6 (4)   10
+     * 当排列行数为 n=4 时：
+     *
+     * 0 (6) 6 (6)       12
+     * 1 (4) 5 (2) 7 (4) 11 (2) 13
+     * 2 (2) 4 (4) 8 (2) 10 (4) 14
+     * 3 (6)       9 (6)        15
+     * 当排列行数为 n=5 时：
+     *
+     * 0 (8) 8 (8)        16 (8)
+     * 1 (6) 7 (2) 9  (6) 15 (2) 17
+     * 2 (4) 6 (4) 10 (4) 14 (4) 18
+     * 3 (2) 5 (6) 11 (2) 13 (6) 19
+     * 4 (8)       12 (8)        20
+     *
+     * 规律如下：
+     * 按行看，
+     * 第 0 行和第 n-1 行的下标间距为 2*(n-1)，
+     * 当 i = 1 到 n-2 时，第 i 行的下标间距为 2*(n - 1 - i) 和 2*i 交替。
+     * 编码技巧如下：
+     * 从上面可以知道，中间行的下标间距是在两个值之间来回变动的。我们可以使用一点小技巧方便的判断当前的间距应该是哪个值，使得代码更简洁优美。
+     * 下面两点小技巧效果是等同的：
+     *
+     * 利用异或运算，假设初始化 a = 0，每次操作完后，令 a = a ^ 1，就可以自动让 a 在 0，1 之间来回变动，我们只需要判断当前的 a 为何值就可知道现在应该是用哪个间距值。
+     * 我们也可以初始化 a = -1，然后每次操作完后，令 a = -a，可以让 a 在 -1，1 之间来回变动，从而实现相同的效果。
+     *
+     */
+    public static String convert1(String s, int numRows) {
+        if(s == null || s.length() == 0 || numRows <= 1) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder(s.length());
+        int a, j;
+        for(int i = 0; i < numRows; ++i) {
+            a = 0;
+            j = i;
+            while (true) {
+                if (j < s.length()) {
+                    sb.append(s.charAt(j));
+                } else break;
+                if ((a ^= 1) == 1) {
+                    j += 2 * ((numRows - i - 1) == 0 ? i : numRows - i - 1);
+                } else {
+                    j += 2 * (i == 0 ? numRows - i - 1 : i);
+                }
+            }
+        }
+        return sb.toString();
     }
 
 }
