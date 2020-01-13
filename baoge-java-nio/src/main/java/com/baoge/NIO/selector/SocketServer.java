52191c87b55f56ea13ad389f1564df9eabd6e958
邵绪宝 <shaoxb@yunjiglobal.com>
2020-01-13 18:22:33 +0800

    支持多路复用IO的服务器端注释

--- baoge-java-nio/src/main/java/com/baoge/NIO/selector/SocketServer.java
+++ baoge-java-nio/src/main/java/com/baoge/NIO/selector/SocketServer.java
@@ -19,6 +19,16 @@ import java.util.concurrent.ConcurrentMap;
  * @Date 2020/1/13 17:54
  *
  * 支持多路复用IO的服务器端
+ * 著作权归https://www.pdai.tech所有。
+ * 链接：https://www.pdai.tech/md/java/io/java-io-nio-select-epoll.html
+ *
+ * 多路复用IO的优缺点 不用再使用多线程来进行IO处理了(包括操作系统内核IO管理模块和应用程序进程而言)。
+ * 当然实际业务的处理中，应用程序进程还是可以引入线程池技术的 同一个端口可以处理多种协议，例如，使用ServerSocketChannel测测的服务器端口监听，
+ * 既可以处理TCP协议又可以处理UDP协议。 操作系统级别的优化: 多路复用IO技术可以是操作系统级别在一个端口上能够同时接受多个客户端的IO事件。
+ * 同时具有之前我们讲到的阻塞式同步IO和非阻塞式同步IO的所有特点。Selector的一部分作用更相当于“轮询代理器”。
+ * 都是同步IO: 目前我们介绍的 阻塞式IO、非阻塞式IO甚至包括多路复用IO，这些都是基于操作系统级别对“同步IO”的实现。我们一直在说“同步IO”，一直都没有详细说，什么叫做“同步IO”。
+ * 实际上一句话就可以说清楚: 只有上层(包括上层的某种代理机制)系统询问我是否有某个事件发生了，否则我不会主动告诉上层系统事件发生了
+ *
  * 可运行NioClient测试
  */
 public class SocketServer {
