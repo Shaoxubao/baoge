acb189419ccea3e59682aeaf2e6a151badfd8027
邵绪宝 <shaoxb@yunjiglobal.com>
2020-01-15 18:01:03 +0800

    PromiseTest

---
+++ baoge-netty/src/main/java/com/baoge/netty_test/PromiseTest.java
@@ -0,0 +1,55 @@
+package com.baoge.netty_test;
+
+import io.netty.util.concurrent.*;
+
+/**
+ * @Author shaoxubao
+ * @Date 2020/1/15 17:57
+ */
+public class PromiseTest {
+    public static void main(String[] args) {
+
+        // 构造线程池
+        EventExecutor executor = new DefaultEventExecutor();
+
+        // 创建 DefaultPromise 实例
+        final Promise promise = new DefaultPromise(executor);
+
+        // 下面给这个 promise 添加两个 listener
+        promise.addListener(new GenericFutureListener<Future<Integer>>() {
+            @Override
+            public void operationComplete(Future future) throws Exception {
+                if (future.isSuccess()) {
+                    System.out.println("任务结束，结果：" + future.get());
+                } else {
+                    System.out.println("任务失败，异常：" + future.cause());
+                }
+            }
+        }).addListener(new GenericFutureListener<Future<Integer>>() {
+            @Override
+            public void operationComplete(Future future) throws Exception {
+                System.out.println("任务结束，balabala...");
+            }
+        });
+
+        // 提交任务到线程池，五秒后执行结束，设置执行 promise 的结果
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                }
+                // 设置 promise 的结果
+                // promise.setFailure(new RuntimeException());
+                promise.setSuccess(123456);
+            }
+        });
+
+        // main 线程阻塞等待执行结果
+        try {
+            promise.sync();
+        } catch (InterruptedException e) {
+        }
+    }
+}
