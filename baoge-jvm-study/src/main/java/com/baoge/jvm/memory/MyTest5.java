961c27ff0ed800879b30389ea801057766c5de71
shaoxubao <xubao_shao@163.com>
2020-03-14 20:52:43 +0800

    jcmd

--- baoge-jvm-study/src/main/java/com/baoge/jvm/memory/MyTest5.java
+++ baoge-jvm-study/src/main/java/com/baoge/jvm/memory/MyTest5.java
@@ -9,6 +9,13 @@ package com.baoge.jvm.memory;
  * jmap -heap pid     堆信息
  * jstat -gc pid 打印元空间信息(MC MU)
  * jcmd PID GC.class_stats 一个新的诊断命令，用来连接到运行的 JVM 并输出详尽的类元数据的柱状图。
+ * jcmd PID VM.flags 查看jvm设置的参数(jvm启动参数)，如最大最小堆，gc收集器等
+ * jcmd PID help 查看可对当前进程进行如下参数操作
+ * jcmd PID VM.uptime 查看JVM的启动时长
+ * jcmd PID GC.class_histogram 查看系统中类的统计信息
+ * jcmd PID Thread.print 查看当前线程堆栈信息
+ * jcmd PID GC.heap_dump [文件路径名]  导出堆栈信息
+ * jcmd PID VM.system_propertis 查看JVM的属性信息
  *
  */
 public class MyTest5 {
