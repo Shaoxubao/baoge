73b5b6a1142d68c6cba6253a38f5cebd9ff22e44
shaoxubao <xubao_shao@163.com>
2020-03-15 11:07:30 +0800

    jcmd

--- baoge-jvm-study/src/main/java/com/baoge/jvm/memory/MyTest5.java
+++ baoge-jvm-study/src/main/java/com/baoge/jvm/memory/MyTest5.java
@@ -16,7 +16,14 @@ package com.baoge.jvm.memory;
  * jcmd PID Thread.print 查看当前线程堆栈信息
  * jcmd PID GC.heap_dump [文件路径名]  导出堆栈信息
  * jcmd PID VM.system_propertis 查看JVM的属性信息
+ * jcmd PID VM.command_line JVM启动时命令参数
  *
+ * jstack:可以查看或者导出Java应用程序中线程的堆栈信息
+ * jstack PID:打印线程堆栈信息
+ *
+ * jmc:Java Misson Control 实时获取Java运行程序信息
+ *
+ * jhat:分析堆转储文件
  */
 public class MyTest5 {
     public static void main(String[] args) {
