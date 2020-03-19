d91b73e1ae96de768eb2895861bfea86e3e60d46
邵绪宝 <shaoxb@yunjiglobal.com>
2020-03-19 17:54:37 +0800

    命令注释

--- baoge-jvm-study/src/main/java/com/baoge/jvm/memory/MyTest5.java
+++ baoge-jvm-study/src/main/java/com/baoge/jvm/memory/MyTest5.java
@@ -7,16 +7,18 @@ package com.baoge.jvm.memory;
  *
  * jmap -clstats pid  查看类加载器信息
  * jmap -heap pid     堆信息
- * jstat -gc pid 打印元空间信息(MC MU)
- * jcmd PID GC.class_stats 一个新的诊断命令，用来连接到运行的 JVM 并输出详尽的类元数据的柱状图。
- * jcmd PID VM.flags 查看jvm设置的参数(jvm启动参数)，如最大最小堆，gc收集器等
- * jcmd PID help 查看可对当前进程进行如下参数操作
- * jcmd PID VM.uptime 查看JVM的启动时长
+ * jstat -gc pid      打印元空间信息(MC MU)
+ * jstat -gcutil pid 1000 每秒钟打印一次gc情况
+ * jmap -dump:format=b,file=heap.hprof pid 导出堆内存溢出文件
+ * jcmd PID GC.class_stats  一个新的诊断命令，用来连接到运行的 JVM 并输出详尽的类元数据的柱状图。
+ * jcmd PID VM.flags        查看jvm设置的参数(jvm启动参数)，如最大最小堆，gc收集器等
+ * jcmd PID help            查看可对当前进程进行如下参数操作
+ * jcmd PID VM.uptime       查看JVM的启动时长
  * jcmd PID GC.class_histogram 查看系统中类的统计信息
- * jcmd PID Thread.print 查看当前线程堆栈信息
+ * jcmd PID Thread.print       查看当前线程堆栈信息
  * jcmd PID GC.heap_dump [文件路径名]  导出堆栈信息
- * jcmd PID VM.system_propertis 查看JVM的属性信息
- * jcmd PID VM.command_line JVM启动时命令参数
+ * jcmd PID VM.system_propertis      查看JVM的属性信息
+ * jcmd PID VM.command_line          JVM启动时命令参数
  *
  * jstack:可以查看或者导出Java应用程序中线程的堆栈信息
  * jstack PID:打印线程堆栈信息
