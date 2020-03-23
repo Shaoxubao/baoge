edb8065b7a7f4af66e6e2d860c79bed77d15ee20
邵绪宝 <shaoxb@yunjiglobal.com>
2020-03-23 17:55:12 +0800

    zk 分布式锁

---
+++ baoge-java/src/main/java/com/baoge/distribute_lock/zookeeper/v2/Test.java
@@ -0,0 +1,38 @@
+package com.baoge.distribute_lock.zookeeper.v2;
+
+/**
+ * @Author shaoxubao
+ * @Date 2020/3/23 17:30
+ */
+public class Test {
+
+    static int n = 500;
+
+    public static void secskill() {
+        System.out.println(--n);
+    }
+
+    public static void main(String[] args) {
+
+        Runnable runnable = new Runnable() {
+            public void run() {
+                DistributedLockV2 lock = null;
+                try {
+                    lock = new DistributedLockV2("127.0.0.1:2181", "test1");
+                    lock.lock();
+                    secskill();
+                    System.out.println(Thread.currentThread().getName() + "正在运行");
+                } finally {
+                    if (lock != null) {
+                        lock.unlock();
+                    }
+                }
+            }
+        };
+
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(runnable);
+            t.start();
+        }
+    }
+}
