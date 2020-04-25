132a4a4f26fb2909eb99eefa5ba81c488d41ac2a
邵绪宝 <shaoxb@yunjiglobal.com>
2020-04-25 11:12:52 +0800

    countdownlatch

---
+++ baoge-java/src/main/java/com/baoge/utils/MyJsonUtils.java
@@ -0,0 +1,21 @@
+package com.baoge.utils;
+
+/**
+ * @Author shaoxubao
+ * @Date 2020/4/24 10:31
+ */
+
+/**
+ * 对象转JSON
+ */
+public class MyJsonUtils {
+
+    public static String parseBeanToJson(Object object) {
+        if (object == null) {
+            return "";
+        }
+
+        return "";
+    }
+
+}
