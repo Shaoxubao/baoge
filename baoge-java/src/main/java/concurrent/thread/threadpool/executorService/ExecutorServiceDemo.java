35dac3882f36830e8c69dd20fa8b3473545358d8
邵绪宝 <shaoxb@yunjiglobal.com>
2020-04-29 09:44:41 +0800

    线程池参数

--- baoge-java/src/main/java/concurrent/thread/threadpool/executorService/ExecutorServiceDemo.java
+++ baoge-java/src/main/java/concurrent/thread/threadpool/executorService/ExecutorServiceDemo.java
@@ -9,21 +9,32 @@ import java.util.concurrent.*;
 public class ExecutorServiceDemo {
 
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(3);
+//        ExecutorService executor = Executors.newFixedThreadPool(3);
+        ExecutorService executor = new ThreadPoolExecutor(3, 3, 0L,
+                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
         CallableHandle task1 = new CallableHandle(1);
         CallableHandle task2 = new CallableHandle(2);
         CallableHandle task3 = new CallableHandle(3);
+        CallableHandle task4 = new CallableHandle(4);
 
         Future<Integer> future1 = executor.submit(task1);
         Future<Integer> future2 = executor.submit(task2);
         Future<Integer> future3 = executor.submit(task3);
+        Future<Integer> future4 = executor.submit(task4);
 
         try {
+            System.out.println("线程池活跃线程数：" + ((ThreadPoolExecutor) executor).getActiveCount());
+
             Integer r1 = future1.get();
             Integer r2 = future2.get();
             Integer r3 = future3.get();
+            Integer r4 = future4.get();
 
-            System.out.println(r1 + r2 + r3);
+            System.out.println("result :" + r1 + r2 + r3 + r4);
+
+            System.out.println("线程池线程总数：" + ((ThreadPoolExecutor) executor).getPoolSize());
+            System.out.println("线程池核心线程数：" + ((ThreadPoolExecutor) executor).getCorePoolSize());
+            System.out.println("线程池最大线程数：" + ((ThreadPoolExecutor) executor).getMaximumPoolSize());
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -42,12 +53,16 @@ public class ExecutorServiceDemo {
 
         // 实现Callable接口的call方法,会在线程池submit(this)的时候调用本方法
         public Integer call() {
+            System.out.println("当前线程：" + Thread.currentThread().getName());
+
             if (1 == this.taskId) {
                 return 1;
             } else if (2== this.taskId) {
                 return 2;
             } else if (3== this.taskId) {
                 return 3;
+            } else if (4== this.taskId) {
+                return 4;
             }
 
             return 0;
