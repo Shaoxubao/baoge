21662b2908a6226b500673fd184e4f62d923bf01
邵绪宝 <shaoxb@yunjiglobal.com>
2020-04-29 10:56:15 +0800

    类型修改

--- baoge-java/src/main/java/concurrent/thread/threadpool/executorService/ExecutorServiceDemo.java
+++ baoge-java/src/main/java/concurrent/thread/threadpool/executorService/ExecutorServiceDemo.java
@@ -1,42 +1,62 @@
 package concurrent.thread.threadpool.executorService;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
 import java.util.concurrent.*;
 
 /**
  * @Author shaoxubao
  * @Date 2020/4/14 10:07
+ *
+ * JDK 提供了四种内置拒绝策略：
+ * 1、DiscardPolicy: 默默丢弃无法处理的任务，不予任何处理
+ * 2、DiscardOldestPolicy: 丢弃队列中最老的任务, 尝试再次提交当前任务
+ * 3、AbortPolicy: 直接抛异常，阻止系统正常工作。
+ * 4、CallerRunsPolicy: 将任务分给调用线程来执行,运行当前被丢弃的任务，这样做不会真的丢弃任务，但是提交的线程性能有可能急剧下降。
  */
 public class ExecutorServiceDemo {
 
     public static void main(String[] args) {
 //        ExecutorService executor = Executors.newFixedThreadPool(3);
-        ExecutorService executor = new ThreadPoolExecutor(3, 3, 0L,
-                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+//        ExecutorService executor = new ThreadPoolExecutor(1, 2, 0L,
+//                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+                .setNameFormat("thread-pool-%d").build();
+        ExecutorService executor = new ThreadPoolExecutor(1, 2, 0L,
+                TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(2), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy()); // 有界队列
         CallableHandle task1 = new CallableHandle(1);
         CallableHandle task2 = new CallableHandle(2);
         CallableHandle task3 = new CallableHandle(3);
         CallableHandle task4 = new CallableHandle(4);
-
-        Future<Integer> future1 = executor.submit(task1);
-        Future<Integer> future2 = executor.submit(task2);
-        Future<Integer> future3 = executor.submit(task3);
-        Future<Integer> future4 = executor.submit(task4);
+        CallableHandle task5 = new CallableHandle(5);
 
         try {
-            System.out.println("线程池活跃线程数：" + ((ThreadPoolExecutor) executor).getActiveCount());
+            // 打印线程池状态
+            printThreadPoolState(executor);
+
+            Future<Integer> future1 = executor.submit(task1);
+            Future<Integer> future2 = executor.submit(task2);
+            Future<Integer> future3 = executor.submit(task3);
+            Future<Integer> future4 = executor.submit(task4);
+            Future<Integer> future5 = executor.submit(task5);
+
+            // 打印线程池状态
+            printThreadPoolState(executor);
 
             Integer r1 = future1.get();
             Integer r2 = future2.get();
             Integer r3 = future3.get();
             Integer r4 = future4.get();
+            Integer r5 = future5.get();
 
-            System.out.println("result :" + r1 + r2 + r3 + r4);
-
-            System.out.println("线程池线程总数：" + ((ThreadPoolExecutor) executor).getPoolSize());
-            System.out.println("线程池核心线程数：" + ((ThreadPoolExecutor) executor).getCorePoolSize());
-            System.out.println("线程池最大线程数：" + ((ThreadPoolExecutor) executor).getMaximumPoolSize());
+            System.out.println("result :" + (r1 + r2 + r3 + r4 + r5));
+            // 打印线程池状态
+            printThreadPoolState(executor);
         } catch (Exception e) {
             e.printStackTrace();
+            // 打印线程池状态
+            printThreadPoolState(executor);
         } finally {
             executor.shutdown(); // 关闭线程池
         }
@@ -55,18 +75,42 @@ public class ExecutorServiceDemo {
         public Integer call() {
             System.out.println("当前线程：" + Thread.currentThread().getName());
 
-            if (1 == this.taskId) {
-                return 1;
-            } else if (2== this.taskId) {
-                return 2;
-            } else if (3== this.taskId) {
-                return 3;
-            } else if (4== this.taskId) {
-                return 4;
+            try {
+                if (1 == this.taskId) {
+                    TimeUnit.SECONDS.sleep(1);
+                    return 1;
+                } else if (2== this.taskId) {
+                    TimeUnit.SECONDS.sleep(1);
+                    return 2;
+                } else if (3== this.taskId) {
+                    TimeUnit.SECONDS.sleep(1);
+                    return 3;
+                } else if (4== this.taskId) {
+                    TimeUnit.SECONDS.sleep(1);
+                    return 4;
+                } else if (5== this.taskId) {
+                    TimeUnit.SECONDS.sleep(1);
+                    return 5;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
             return 0;
         }
+    }
+
+    public static void printThreadPoolState(ExecutorService executor) {
+        System.out.println("=====================================================");
+        System.out.println("线程池正在执行任务线程数：" + ((ThreadPoolExecutor) executor).getActiveCount());
+        System.out.println("线程池线程总数：" + ((ThreadPoolExecutor) executor).getPoolSize());
+        System.out.println("线程池核心线程数：" + ((ThreadPoolExecutor) executor).getCorePoolSize());
+        System.out.println("最大允许的线程数：" + ((ThreadPoolExecutor) executor).getMaximumPoolSize());
+        System.out.println("线程空闲时间：" + ((ThreadPoolExecutor) executor).getKeepAliveTime(TimeUnit.MILLISECONDS));
+        System.out.println("任务总数：" + ((ThreadPoolExecutor) executor).getTaskCount());
+        System.out.println("已完成任务数量：" + ((ThreadPoolExecutor) executor).getCompletedTaskCount());
+        System.out.println("队列里缓存的任务数量：" + ((ThreadPoolExecutor) executor).getQueue().size());
+        System.out.println("池中存在的最大线程数：" + ((ThreadPoolExecutor) executor).getLargestPoolSize());
     }
 
 }
