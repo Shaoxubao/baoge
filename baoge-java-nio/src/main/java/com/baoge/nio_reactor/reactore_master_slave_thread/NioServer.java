ffcfd725bbec5fde0d2584c83b4acf9101ed7c46
邵绪宝 <shaoxb@yunjiglobal.com>
2020-06-24 14:59:58 +0800

    优化

--- baoge-java-nio/src/main/java/com/baoge/nio_reactor/reactore_master_slave_thread/NioServer.java
+++ baoge-java-nio/src/main/java/com/baoge/nio_reactor/reactore_master_slave_thread/NioServer.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Executors;
  *
  * Reactor 主从Reactor模式实现(Reactor反应堆设计模式实现（基于java.nio）)
  *
- * Acceeptor：职责维护java.nio.ServerSocketChannel类，绑定服务端监听端口，然后将该通道注册到MainRector中；
+ * Acceptor：职责维护java.nio.ServerSocketChannel类，绑定服务端监听端口，然后将该通道注册到MainRector中；
  * Main Reactor,监听客户端连接的反应堆，这里使用jdk并发中的Executors.newSingleThreadExecutor线程池来实现，监听客户端的连接事件(OP_ACCEPT)
  * Sub Reactor,目前没有使用jdk的并发池，这里用的SubReactorThreadGroup,其实现是数组，当然这里也可以使用jdk线程池，
  * SubReactor的每一个线程都是IO线程，用来处理读，写事件。所有的IO线程公用一个业务线程池（基于juc）实现，用来处理业务逻辑，也就是运行Handel的地方。
