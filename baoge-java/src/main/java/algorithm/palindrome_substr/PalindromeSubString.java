532eea09a06b7e596bbc1472b70d4a7da493aac6
邵绪宝 <shaoxb@yunjiglobal.com>
2020-07-21 15:03:54 +0800

    回文串

--- baoge-java/src/main/java/algorithm/palindrome_substr/PalindromeSubString.java
+++ baoge-java/src/main/java/algorithm/palindrome_substr/PalindromeSubString.java
@@ -21,9 +21,76 @@ import java.util.Map;
 public class PalindromeSubString {
 
     public static void main(String[] args) {
+        System.out.println(longestPalindrome1("acdbbdaalkmmmjmmmkl"));
+        System.out.println(longestPalindrome2("acdbbdaa"));
         System.out.println(longestPalindromeSubString("babad"));
         System.out.println(longestPalindrome("babad"));
         System.out.println(longestPalindromeByDynamicProgramming("babad"));
+    }
+
+    public static String longestPalindrome1(String s) {
+
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        int strLen = s.length();
+        int left = 0;
+        int right = 0;
+        int len = 1;
+        int maxStart = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < strLen; i++) {
+            left = i - 1;
+            right = i + 1;
+            while (left >= 0 && s.charAt(left) == s.charAt(i)) {
+                len++;
+                left--;
+            }
+            while (right < strLen && s.charAt(right) == s.charAt(i)) {
+                len++;
+                right++;
+            }
+            while (left >= 0 && right < strLen && s.charAt(right) == s.charAt(left)) {
+                len = len + 2;
+                left--;
+                right++;
+            }
+            if (len > maxLen) {
+                maxLen = len;
+                maxStart = left;
+            }
+            len = 1;
+        }
+        return s.substring(maxStart + 1, maxStart + maxLen + 1);
+
+    }
+
+    public static String longestPalindrome2(String s) {
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+        int strLen = s.length();
+        int maxStart = 0;  // 最长回文串的起点
+        int maxEnd = 0;    // 最长回文串的终点
+        int maxLen = 1;    // 最长回文串的长度
+
+        boolean[][] dp = new boolean[strLen][strLen];
+
+        for (int r = 1; r < strLen; r++) {
+            for (int l = 0; l < r; l++) {
+                if (s.charAt(l) == s.charAt(r) && (r - l <= 2 || dp[l + 1][r - 1])) {
+                    dp[l][r] = true;
+                    if (r - l + 1 > maxLen) {
+                        maxLen = r - l + 1;
+                        maxStart = l;
+                        maxEnd = r;
+                    }
+                }
+            }
+        }
+        return s.substring(maxStart, maxEnd + 1);
+
     }
 
     public static String longestPalindromeSubString(String input) {
@@ -70,7 +137,7 @@ public class PalindromeSubString {
     }
 
     public static String longestPalindrome(String s) {
-        if(s.isEmpty()) {
+        if (s.isEmpty()) {
             return s;
         }
         String res = s.substring(0, 1);
@@ -104,8 +171,8 @@ public class PalindromeSubString {
         for (int i = n - 2; i >= 0; i--) {
             dp[i][i] = true;
             for (int j = i + 1; j < n; j++) {
-                // 3  4             3              4       4   3           3      4
-                dp[i][j] = s.charAt(i) == s.charAt(j) && ( j - i < 3 || dp[i + 1][j - 1]); // 小于3是因为aba一定是回文
+                // 3  4             3              4      4   3           3      4
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]); // 小于3是因为aba一定是回文
                 if (dp[i][j] && right - left < j - i) {
                     left = i;
                     right = j;
