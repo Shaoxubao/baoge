f3af8af039acf7509b077b2068cc1c601dc46c0a
邵绪宝 <shaoxb@yunjiglobal.com>
2020-07-21 15:05:26 +0800

    注释

--- baoge-java/src/main/java/algorithm/palindrome_substr/PalindromeSubString.java
+++ baoge-java/src/main/java/algorithm/palindrome_substr/PalindromeSubString.java
@@ -66,6 +66,13 @@ public class PalindromeSubString {
 
     }
 
+    /**
+     * 上面中心扩散的方法，其实做了很多重复计算。动态规划就是为了减少重复计算的问题。动态规划听起来很高大上。其实说白了就是空间换时间，将计算结果暂存起来，
+     * 避免重复计算。作用和工程中用 redis 做缓存有异曲同工之妙。 我们用一个 boolean dp[l][r] 表示字符串从 i 到 j 这段是否为回文。
+     * 试想如果 dp[l][r]=true，我们要判断 dp[l-1][r+1] 是否为回文。只需要判断字符串在(l-1)和（r+1)两个位置是否为相同的字符，是不是减少了很多重复计算。
+     * 进入正题，动态规划关键是找到初始状态和状态转移方程。 初始状态，l=r 时，此时 dp[l][r]=true。
+     * 状态转移方程，dp[l][r]=true 并且(l-1)和（r+1)两个位置为相同的字符，此时 dp[l-1][r+1]=true。
+     */
     public static String longestPalindrome2(String s) {
         if (s == null || s.length() < 2) {
             return s;
