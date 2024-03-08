189fcf16fa6426b02b5e8d3949b6e9375831422c
shaoxubao <xubao_shao@163.com>
2024-03-08 11:35:02 +0800

    poi excel

--- baoge-excel/src/main/java/com/baoge/ExcelDemo.java
+++ baoge-excel/src/main/java/com/baoge/ExcelDemo.java
@@ -4,6 +4,10 @@ import com.baoge.utils.ExcelUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ExcelDemo {
     public static void main(String[] args) throws IOException, InvalidFormatException {
@@ -12,8 +16,14 @@ public class ExcelDemo {
 //        ExcelUtils.fillData(sourceFilePath, 0, 0, 0, "hello",
 //                targetFilePath);
 
-        String[] rowDataArr = {"world", "world", "world", "world", "world", "world", "world", "world"};
-        ExcelUtils.fillData2(sourceFilePath, 0, 1, 0, rowDataArr,
+//        String[] rowDataArr = {"world", "world", "world", "world", "world", "world", "world", "world"};
+//        ExcelUtils.fillData2(sourceFilePath, 0, 1, 0, rowDataArr,
+//                targetFilePath);
+
+        sourceFilePath = "D:\\负荷.xlsx";
+        Map<String, List<String>> rowDataMap = new LinkedHashMap<>();
+        rowDataMap.put("8203300324", Arrays.asList("51.92", "51.98", "52.00", "52.12", "52.23", "52.23", "52.29", "52.11", "52.04", "51.92", "51.83", "51.72", "51.73", "51.83", "51.91", "51.84", "51.80", "51.74", "51.92", "52.03", "52.20", "52.16", "52.18", "52.03", "51.87", "51.77", "51.74", "51.68", "51.66", "51.79", "51.96", "51.92", "52.09", "52.03", "52.16", "52.09", "52.37", "52.33", "52.47", "52.40", "52.29", "52.17", "52.22", "52.12", "52.20", "52.34", "52.07", "52.09", "52.04", "52.02", "52.27", "52.32", "52.46", "52.62", "52.58", "52.50", "52.38", "52.40", "52.29", "52.35", "52.45", "52.45", "52.46", "52.56", "52.60", "52.58", "52.66", "52.87", "52.92", "52.72", "52.82", "52.60", "52.37", "52.33", "52.27", "52.20", "52.56", "52.47", "52.46", "52.40", "52.46", "52.66", "52.57", "52.90", "52.82", "52.73", "52.61", "52.55", "52.44", "52.37", "52.36", "52.40", "52.15", "52.48", "52.40", "52.26"));
+        ExcelUtils.fillData3(sourceFilePath, 1, 1, 0, rowDataMap, 4,
                 targetFilePath);
     }
 }
--- baoge-excel/src/main/java/com/baoge/utils/ExcelUtils.java
+++ baoge-excel/src/main/java/com/baoge/utils/ExcelUtils.java
@@ -12,6 +12,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class ExcelUtils {
 
@@ -73,6 +74,63 @@ public class ExcelUtils {
             font.setColor(IndexedColors.RED.index);
             cellStyle.setFont(font);
             cell.setCellStyle(cellStyle);
+        }
+        // 修改模板内容导出新模板
+        FileOutputStream out = new FileOutputStream(targetFilePath);
+        // 关闭相应的流
+        wb.write(out);
+        out.close();
+        wb.close();
+    }
+
+    /**
+     * 指定行列填充数据(从上往下填充，列固定)
+     * @param sourceFilePath 文件路径
+     * @param rowNum  行列分别从0开始
+     * @param cellNum 列号
+     * @param sheetIndex 工作区索引
+     * @param rowDataMap 填充的数据
+     * @param rowNumBegin 从的第几行开始填充数据
+     * @param targetFilePath 保存目标文件路径
+     */
+    public static void fillData3(String sourceFilePath, int rowNum, int cellNum, int sheetIndex,
+                                 Map<String, List<String>> rowDataMap, int rowNumBegin,
+                                 String targetFilePath) throws IOException, InvalidFormatException {
+        // 获取Excel模板文件
+        File file = new File(sourceFilePath);
+        // 读取Excel模板
+        XSSFWorkbook wb = new XSSFWorkbook(file);
+        // 读取了模板内sheet的内容
+        XSSFSheet sheet = wb.getSheetAt(sheetIndex);
+        // 在相应的单元格进行（读取）赋值 行列分别从0开始
+        for (int i = cellNum; i < 100; i = i + 4) {
+            XSSFCell cell = sheet.getRow(rowNum).getCell(i);
+            if (cell == null) {
+                break;
+            }
+            // 先读取用户no
+            String cellValue = cell.getStringCellValue();
+            List<String> numbers = StringUtils.extractNumberInBracket(cellValue);
+            String consNo = numbers.get(0);
+            System.out.println(consNo);
+            // 根据用户no拿到数据填充
+            List<String> data = rowDataMap.get(consNo);
+            if (data == null) {
+                continue;
+            }
+            for (int j = 0; j < 96; j++) {
+                XSSFCell cellToFill = sheet.getRow(rowNumBegin).getCell(i);
+                cellToFill.setCellValue(data.get(j));
+                CellStyle cellStyle = wb.createCellStyle();
+                Font font = wb.createFont();
+                font.setFontName("宋体");
+                font.setFontHeightInPoints((short) 12); // 字体大小
+                font.setColor(IndexedColors.BRIGHT_GREEN.index);
+                cellStyle.setFont(font);
+                cellToFill.setCellStyle(cellStyle);
+
+                rowNumBegin = rowNumBegin + 1;
+            }
         }
         // 修改模板内容导出新模板
         FileOutputStream out = new FileOutputStream(targetFilePath);
